0253949d7b0775f3083b3ccb88e9ab9c19c876f6
Ricardo Piro-Rael <ricardo@hint.com>
2020-06-25 02:33:59 -0600

    Integrate with scrapers

--- covid19_sfbayarea/data/__init__.py
+++ covid19_sfbayarea/data/__init__.py
@@ -1,14 +1,15 @@
 from typing import Dict, Any
 from . import alameda
 from . import san_francisco
+from . import san_mateo
 
 scrapers: Dict[str, Any] = {
     'alameda': alameda,
     # 'contra_costa': None,
     # 'marin': None,
     # 'napa': None,
-    'san_francisco': san_francisco
-    # 'san_mateo': None,
+    'san_francisco': san_francisco,
+    'san_mateo': san_mateo,
     # 'santa_clara': None,
     # 'solano': None,
     # 'sonoma': None,
